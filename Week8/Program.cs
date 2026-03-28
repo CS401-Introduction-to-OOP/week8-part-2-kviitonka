3ac0048b2f4f4d36ef8d421585bd9905c2a90321
kviitonka <polinagushevata@gmail.com>
2026-03-28 16:29:15 +0200

    Stage 3

--- Week8/Program.cs
+++ Week8/Program.cs
@@ -1,9 +1,16 @@
-﻿
-class Program
-{
-    public static void Main()
-    {
-        
-    }
-    
-}
+﻿using Week8;
+
+// Value type demo
+Point p1 = new Point { X = 1, Y = 2 };
+Point p2 = p1; // Тут у нас створюється копія саме значень, що містяться в р1. Тому при модифікації р2 р1 ніяк не зміниться, бо вони не залежні.
+p2.X = 3;
+p1.Print();
+p2.Print();
+// Reference type demo
+PointRef r1 = new PointRef { X = 1, Y = 2 };
+PointRef r2 = r1;// Тут у нас створюється посилання на r1, тому r1 i r2 залежні і тому при модифікації r2 r1 також змінюється.
+r2.X = 4;
+r1.Print();
+r2.Print();
+// Boxing demo
+BoxingTester.Test();
