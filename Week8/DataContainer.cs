ce7552f07d4b0540558c64dfc7ff70b97137894f
kviitonka <polinagushevata@gmail.com>
2026-03-28 16:37:52 +0200

    Stage 2

---
+++ Week8/DataContainer.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+namespace Week8;
+
+public class DataContainer<T> where T : DataItem
+{
+    private readonly List<T> _items = new();
+    public void AddItem(T item)
+    {
+        _items.Add(item);
+    }
+    public long GetTotalSize()
+    {
+        long totalSize = 0;
+        foreach (var item in _items)
+        {
+            totalSize += item.GetSize();
+        }
+
+        return totalSize;
+    }
+}
